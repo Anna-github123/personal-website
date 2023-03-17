b9f1bb85b11384975e23218f762fa6a24fdff715
Anna-github123 <73287064+Anna-github123@users.noreply.github.com>
2023-03-17 09:55:32 +0400

    Add files via upload

---
+++ personal-website.php
@@ -0,0 +1,5 @@
+<?php
+            echo " Welcome and Congratulations! ";
+            echo " You have now successfully conpleted the form. Thank you for your enquiry and you would be contacted within Three (3) working days. ";
+            echo " Thank You for using CSS Genius! ";
+        ?>
